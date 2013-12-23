9ad25536fb74d1ab80c65d9a799e447ceeeee201
mattdesl <dave.des@gmail.com>
2013-12-23 02:21:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+//This is an asset loader queue tailored for Kami/WebGL
+
+var LoaderBase = require('assetloader');
+var Class = require('klasse');
+var Texture = require('kami').Texture;
+
+//this is a kami-specific loader for a Texture object
+//it is assumed that the AssetManager being passed is a WebGLContext
+function TextureLoader(name, path, texture, genMipmaps) {
+    path = path || name;
+
+    texture = texture || new Texture(this.context);
+    
+    return {
+
+        value: texture,
+
+        load: function(finished) {
+            var img = new Image(); 
+
+            img.onload = function() {
+                img.onerror = img.onabort = null; //clear other listeners
+                texture.uploadImage(img, undefined, undefined, genMipmaps);
+                finished();
+            };
+            img.onerror = function() {
+                img.onload = img.onabort = null;
+                console.warn("Error loading image: "+path);
+                //We use null data to avoid WebGL errors
+                texture.uploadData(1, 1, undefined, undefined, undefined, genMipmaps); 
+                finished(false);
+            };
+            img.onabort = function() {
+                img.onload = img.onerror = null;
+                console.warn("Aborted image: "+path);
+                //We use null data to avoid WebGL errors
+                texture.uploadData(1, 1, undefined, undefined, undefined, genMipmaps); 
+                finished(false);
+            };
+            //setup source
+            img.src = path;
+        }
+    }
+}
+
+//Setup loader parameters
+TextureLoader.extensions = ["png", "gif", "jpg", "jpeg"];
+TextureLoader.mediaType = "image";
+
+var AssetLoader = new Class({
+
+    Extends: LoaderBase,
+
+    initialize: function(context) {
+        LoaderBase.call(this);
+
+        if (!context)
+            throw "Kami AssetLoader must be passed with a valid WebGLContext";
+
+        this.registerLoader(TextureLoader);
+
+        this.context = context;
+        this.__invalidateFunc = this.invalidate.bind(this);
+        this.context.restored.add( this.__invalidateFunc );
+    },
+
+    destroy: function() {
+        this.context.restored.remove(this.__invalidateFunc);
+    }
+});
+
+//Copy static attributes ...
+AssetLoader.Status = LoaderBase.Status;
+
+AssetLoader.registerCommonLoader = LoaderBase.registerCommonLoader;
+
+AssetLoader.Descriptor = LoaderBase.Descriptor;
+
+AssetLoader.TextureLoader = TextureLoader;
+
+module.exports = AssetLoader;
+
+
+
+
+
+
+
+
+/// old docs. 
+
+    /**
+     * This is the loading function of a AssetManager plugin, 
+     * which handles the asynchronous loading for an asset. 
+     * The function must be implemented in a very
+     * strict manner for the asset manager to work correctly.
+     *
+     * Once the async loading is done, you must call the `finished` callback
+     * that was passed to this method. You can pass the parameter `false` to the
+     * finished callback to indicate the async load has failed. Otherwise, it is assumed
+     * to be successful.
+     * 
+     * If you don't invoke the callback, the asset manager may never finish loading.
+     * 
+     * @param  {String} name the name of the asset to load
+     * @param {Function} finished the function to call when async loading is complete
+     * @param {Texture} texture the texture to operate on for this asset
+     * @param {String} path the optional image path to use instead of the `name` parameter
+     */
+/**
+ * This is the default implementation of an image loader plugin for AssetManager.
+ * This uses a DOM Image object to upload PNG, GIF and JPG images to a WebGL
+ * texture. You will not need to deal with this class directly, unless you want
+ * to write your own AssetManager loaders.
+ *
+ * A Loader plugin is a class which handles the asynchronous loading and provides
+ * a convenient return value for the `AssetManager.load()` functions. The loader class
+ * is constructed with the following parameters: first, the WebGLContext this AssetManager
+ * is using, and second, the name of the asset to be loaded. The subsequent arguments are
+ * those that were passed as extra to the `AssetManager.load()` functions.
+ *
+ * A loader must implement a `load()` function, and it's encouraged to also implement a 
+ * `getReturnValue()` function, for convenience.
+ * 
+ * @param {WebGLContext} context the context, passed by AssetManager
+ * @param {String} name the unique key for this asset
+ * @param {String} path the optional path or data URI to use, will default to the name param
+ * @param {Texture} texture an optional texture to act on; if undefined, a new texture
+ *                          will be created
+ */
