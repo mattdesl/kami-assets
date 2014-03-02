84623ba41261d9ad4d11b306a5db14026e7666f0
mattdesl <dave.des@gmail.com>
2014-03-02 13:49:37 -0500

    pulling TextureLoader out of index

---
+++ ext/TextureLoader.js
@@ -0,0 +1,26 @@
+var Texture = require('kami').Texture;
+
+
+//this is a kami-specific loader for a Texture object
+//it is assumed that the AssetManager being passed is a WebGLContext
+function TextureLoader(name, path, texture, genMipmaps) {
+    path = path || name;
+
+    texture = texture || new Texture(this.context);
+    
+    return {
+
+        value: texture,
+
+        load: function(onComplete, onError) {
+            //Texture has a handy function for this sort of thing...
+            texture.setup(path, onComplete, onError, genMipmaps);
+        }
+    }
+}
+
+//Setup loader parameters
+TextureLoader.extensions = ["png", "gif", "jpg", "jpeg"];
+TextureLoader.mediaType = "image";
+
+module.exports = TextureLoader;
--- index.js
+++ index.js
@@ -2,29 +2,7 @@
 
 var LoaderBase = require('assetloader');
 var Class = require('klasse');
-var Texture = require('kami').Texture;
-
-//this is a kami-specific loader for a Texture object
-//it is assumed that the AssetManager being passed is a WebGLContext
-function TextureLoader(name, path, texture, genMipmaps) {
-    path = path || name;
-
-    texture = texture || new Texture(this.context);
-    
-    return {
-
-        value: texture,
-
-        load: function(onComplete, onError) {
-            //Texture has a handy function for this sort of thing...
-            texture.setup(path, onComplete, onError, genMipmaps);
-        }
-    }
-}
-
-//Setup loader parameters
-TextureLoader.extensions = ["png", "gif", "jpg", "jpeg"];
-TextureLoader.mediaType = "image";
+var TextureLoader = require('./ext/TextureLoader');
 
 var AssetLoader = new Class({
 
@@ -37,7 +15,7 @@ var AssetLoader = new Class({
             throw "Kami AssetLoader must be passed with a valid WebGLContext";
 
         this.registerLoader(TextureLoader);
-
+        
         this.context = context;
         this.__invalidateFunc = this.invalidate.bind(this);
         this.context.restored.add( this.__invalidateFunc );
